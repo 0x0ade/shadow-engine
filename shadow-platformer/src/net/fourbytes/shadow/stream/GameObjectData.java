4e962ab4560d93711d0c679d3c5eb236481e2250
AngelDE98 <nuda1998@gmail.com>
2013-08-11 13:24:18 +0200

    STUBS... STUBS EVERYWHERE

--- shadow-platformer/src/net/fourbytes/shadow/stream/GameObjectData.java
+++ shadow-platformer/src/net/fourbytes/shadow/stream/GameObjectData.java
@@ -3,25 +3,56 @@ package net.fourbytes.shadow.stream;
 import net.fourbytes.shadow.Entity;
 import net.fourbytes.shadow.Block;
 import net.fourbytes.shadow.GameObject;
+import net.fourbytes.shadow.TypeBlock;
 
 /**
- * GameObjectData contains all needed {@link Data} to send {@link GameObject}s thru {@link IStream}s.
+ * GameObjectData contains all needed {@link Data} to send {@link GameObject}s thru {@link IStream}s. <br>
+ * It is only a parent class containing an general {@link #pack(GameObject)} method linking to the correspondent subclasses.
  */
-public class GameObjectData extends Data {
+public abstract class GameObjectData<T extends GameObject> extends Data {
 	
 	public GameObjectData() {
+		super();
 	}
 	
 	/**
-	 * Converts an {@link GameObject} to it's {@link GameObjectData}.
+	 * Converts an {@link GameObject} to it's correspondent {@link GameObjectData} (subclass). <br>
+	 * This is the general method linking to the subclasses maintaining the given type.
 	 * @param go {@link GameObject} to convert
 	 * @return {@link Data} to send thru streams.
 	 */
-	public static GameObjectData pack(GameObject go) {
-		// TODO Auto-generated method stub
-		return null;
+	public final static <V extends GameObject> GameObjectData<V> autopack(V go) {
+		GameObjectData<V> god = null;
+		
+		if (go instanceof Entity) {
+			god = (GameObjectData<V>) new EntityData();
+		}
+		if (go instanceof Block) {
+			if (go instanceof TypeBlock) {
+				god = (GameObjectData<V>) new TypeBlockData();
+			} else {
+				god = (GameObjectData<V>) new BlockData();
+			}
+		}
+		
+		if (god != null) { //Well... it will never happen, right?
+			god.pack(go);
+		}
+		
+		return god;
 	}
 	
+	/**
+	 * Converts an {@link GameObject} to it's correspondent {@link GameObjectData}.
+	 * @param go {@link GameObject} to convert
+	 * @return {@link Data} to send thru streams.
+	 */
+	public abstract GameObjectData<T> pack(T go);
 	
+	/**
+	 * Converts this {@link GameObjectData} back to it's correspondent {@link GameObject}.
+	 * @return {@link GameObject} packed in this {@link GameObjectData}.
+	 */
+	public abstract GameObject unpack();
 	
 }
