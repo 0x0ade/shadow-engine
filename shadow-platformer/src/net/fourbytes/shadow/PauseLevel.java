5d6fe030a8c76d4c413ccafffbd383305889c30b
AngelDE98 <nuda1998@gmail.com>
2013-08-16 17:55:57 +0200

    Added basic load and save items to pause menu.

--- shadow-platformer/src/net/fourbytes/shadow/PauseLevel.java
+++ shadow-platformer/src/net/fourbytes/shadow/PauseLevel.java
@@ -6,6 +6,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.utils.Array;
 
 import net.fourbytes.shadow.Input.Key;
+import net.fourbytes.shadow.map.ShadowMap;
 
 public class PauseLevel extends TitleLevel {
 
@@ -22,10 +23,23 @@ public class PauseLevel extends TitleLevel {
 			Shadow.level = bglevel;
 			}}));
 		items.add(new MenuItem(this, "Save", new Runnable(){public void run(){
-			//TODO
+			ShadowMap map = ShadowMap.createFrom(bglevel);
+			map.save(Gdx.files.local("TROLL.smf"));
+			Shadow.level = bglevel;
 			}}));
 		items.add(new MenuItem(this, "Load", new Runnable(){public void run(){
 			//TODO
+			ShadowMap map = ShadowMap.loadFile(Gdx.files.local("TROLL.smf"));
+			for (Layer layer : bglevel.layers.values()) {
+				for (Block b : layer.blocks) {
+					layer.remove(b);
+				}
+				for (Entity e : layer.entities) {
+					layer.remove(e);
+				}
+			}
+			map.fillLevel(bglevel);
+			Shadow.level = bglevel;
 			}}));
 		items.add(new MenuItem(this, "Main Menu", new Runnable(){public void run(){
 			Shadow.level = new TitleLevel();
