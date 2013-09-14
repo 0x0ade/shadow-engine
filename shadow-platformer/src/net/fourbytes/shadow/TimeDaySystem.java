3da4a4c9f8ccdba29f4a037208a96c0bbb947e98
AngelDE98 <nuda1998@gmail.com>
2013-09-14 14:55:32 +0200

    Some TimeDaySystem polishing...

--- shadow-platformer/src/net/fourbytes/shadow/TimeDaySystem.java
+++ shadow-platformer/src/net/fourbytes/shadow/TimeDaySystem.java
@@ -11,21 +11,26 @@ public class TimeDaySystem {
 
 	public Level level;
 	//delta in seconds!
-	public static float fullday = 60f * 5f; //36000 ticks is 20 minutes at 30 FPS
+	public static float fullday = 60f * 10f; //36000 ticks is 20 minutes at 30 FPS
 	//public static float fullday = 8f;
 	public static float day = 0;
-	public float delta = fullday/2f - fullday/5f;
+	//public float delta = fullday/2f - fullday/5f;
+	public float delta = 0f;
 	
 	public static Array<Color> colors = new Array<Color>();
 	public static Array<Color> colorsbg = new Array<Color>();
 	static {
-		colors.add(new Color(0f, 0.1f, 0.25f, 1f));
-		colors.add(new Color(0.7f, 0.6f, 0.4f, 1f));
+		colors.add(new Color(0.125f, 0.25f, 0.5f, 1f));
+		colors.add(new Color(0.125f, 0.25f, 0.5f, 1f));
+		colors.add(new Color(0.125f, 0.25f, 0.5f, 1f));
+		colors.add(new Color(0.6f, 0.5f, 0.25f, 1f));
 		colors.add(new Color(1f, 1f, 1f, 1f));
 		colors.add(new Color(0.7f, 0.6f, 0.3f, 1f));
 		
 		colorsbg.add(new Color(0f, 0.125f, 0.3f, 1f));
-		colorsbg.add(new Color(0.7f, 0.6f, 0.3f, 1f));
+		colorsbg.add(new Color(0f, 0.125f, 0.3f, 1f));
+		colorsbg.add(new Color(0f, 0.125f, 0.3f, 1f));
+		colorsbg.add(new Color(0.6f, 0.5f, 0.25f, 1f));
 		colorsbg.add(new Color(0.2f, 0.5f, 0.7f, 1f));
 		colorsbg.add(new Color(0.7f, 0.6f, 0.3f, 1f));
 	}
@@ -67,7 +72,7 @@ public class TimeDaySystem {
 		level.globalLight.set(tmp);
 		Shadow.cam.bg.c1.set(tmpbg);
 		Shadow.cam.bg.c2.set(tmpbg2).sub(0.2f, 0.2f, 0.2f, 0f);
-		if (ci == 0 || cii == 0) {
+		if ((ci >= 0 || cii >= 0) && (ci <= 2 || cii <= 2)) {
 				if (cii == 0) {
 					Shadow.cam.bg.starsAlpha = (delta-3f*(fullday/4f))/(fullday/4f);
 				} else {
