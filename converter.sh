ab96b56cf5565d2374ae0373c4d5ae894b282a7f
AngelDE98 <nuda1998@gmail.com>
2013-10-15 13:44:32 +0200

    Missing file added: converter.sh Feel free to convert it to a Windoze Batch .bat file...

---
+++ converter.sh
@@ -0,0 +1,3 @@
+java -verbosegc -cp \
+./shadow-platformer/bin:./shadow-platformer-desktop/bin:./shadow-platformer/libs/*:./shadow-platformer-desktop/libs/* \
+net.fourbytes.shadow.ConverterLauncher "$@"
