68167e7574369f0f727bc3a495b174a46abb61c1
AngelDE98 <nuda1998@gmail.com>
2014-05-05 17:50:11 +0200

    Fix some block values not being saved

--- shadow-platformer/src/net/fourbytes/shadow/Block.java
+++ shadow-platformer/src/net/fourbytes/shadow/Block.java
@@ -3,16 +3,21 @@ package net.fourbytes.shadow;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
+import net.fourbytes.shadow.map.Saveable;
 
 public class Block extends GameObject {
 	
 	public String subtype = "";
+	@Saveable
 	public boolean tickInView = false;
+	@Saveable
 	public boolean tickAlways = false;
 	/**
 	 * 0x00 = normal only, 0x01 = rendertop only, anything other = both
 	 */
+	@Saveable
 	public byte rendertop = 0x00;
+	@Saveable
 	public Rectangle colloffs = new Rectangle(0, 0, 0, 0);
 	
 	public Block(Vector2 pos, Layer layer) {
